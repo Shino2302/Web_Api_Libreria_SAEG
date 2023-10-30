e70ce4283257edea2976d58a86399dc263d1356a
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-30 13:07:28 -0700

    Entity Framework

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+
+namespace Libreria_SAEG.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+    }
+}
---
+++ Data/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Threading;
+
+namespace Libreria_SAEG.Data.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRaed { get; set; }
+        public string Rate { get; set; }
+        public string Genero { get; set; }
+        public string Autor { get; set; }
+        public string ConverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
