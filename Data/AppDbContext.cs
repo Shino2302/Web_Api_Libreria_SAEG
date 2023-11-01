c868694beb66ff593806bee2699e76f4bf9fa1e5
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-31 19:07:47 -0700

    Cambios

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Libreria_SAEG.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Policy;
 
 namespace Libreria_SAEG.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
         {
-
+            
         }
+        public DbSet<Book> Books { get; set; }
     }
 }
